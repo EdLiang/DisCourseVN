0b0211fc644dd91b5aea22eac5f9c67790de0980
Madaoism <caiyiming2015@gmail.com>
2019-03-01 14:38:01 -0800

    test

--- navbar.js
+++ navbar.js
@@ -1,10 +1,10 @@
 
 jQuery(document).ready(function(){
 
-    jQuery('#navbar').load('navbar.html', function() {
-        console.log("Load was performed");
-    }, function() {
-        console.log("Error detected");
-    })
+//    jQuery('#navbar').load('navbar.html', function () {
+//        console.log("Load was performed");
+//    }, function () {
+//        console.log("Error detected");
+//    })
 
 });
