e78a733223e363efb6c5347275496d0493461fb2
Madaoism <caiyiming2015@gmail.com>
2019-03-01 23:38:37 -0800

    allowed resizing then hiding navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,20 @@
+let handleResize = function() {
+
+    "use strict";
+
+    var mobile = (/iphone|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));          
+    let aspectRatio = window.innerWidth / window.innerHeight;
+
+    if (mobile || aspectRatio > 2.0 || aspectRatio < 1.2)
+    {
+        $('#navContainer').css('display', 'none');
+    }
+    else
+    {
+        $('#navContainer').css('display', 'block');
+    }
+}
+
 $(document).ready(function () {
     "use strict";
 
@@ -7,17 +24,20 @@ $(document).ready(function () {
 
         document.getElementById("navbar").innerHTML = navbar;
 
-        let filename = document.location.pathname.match(/[^\/]+$/)[0];
-        let navHeaderId = "nav_" + filename;
+        let filename = document.location.pathname.match(/[^\/]+$/);
+        
+        if (filename === null) filename = "index.html";
+        else filename = filename[0];
 
-        document.getElementById(navHeaderId).classList.add("SelectedLink")
+        let navHeaderId = "nav_" + filename;
+        //document.getElementById(navHeaderId).classList.add("SelectedLink")
+
+        // check for the first time if the navbar needs to be hidden
+        handleResize();
 
     }, 'text');
- 
+
+    $(window).resize(handleResize);
+
 });
 
-let elements = document.getElementsByClassName("navAnchors");
-for (let i = 0; i < elements.length; i++) {
-    let cs = window.getComputedStyle(elements[i]);
-    let actualSize = Number.parseFloat(cs.fontSize) * detectZoom.zoom();
-}
