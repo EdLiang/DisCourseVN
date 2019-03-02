e38eae1e26ebbd95ad8d7862c22a059ea317b698
Madaoism <caiyiming2015@gmail.com>
2019-03-02 00:14:26 -0800

    fixed navbar

--- js/navbar.js
+++ js/navbar.js
@@ -5,8 +5,9 @@ let handleResize = function() {
     var mobile = (/iphone|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));          
     let aspectRatio = window.innerWidth / window.innerHeight;
 
-    if (mobile || aspectRatio > 2.0 || aspectRatio < 1.2)
+    if (mobile || aspectRatio > 3.0 || aspectRatio < 1.2)
     {
+        console.log(aspectRatio);
         $('#navContainer').css('display', 'none');
     }
     else
@@ -19,10 +20,12 @@ $(document).ready(function () {
     "use strict";
 
     let navbar = "";
+    var navHide = true;
+    
     $.get('../navbar.html', function(data) {
         navbar = data;
 
-        document.getElementById("navbar").innerHTML = navbar;
+        document.getElementById("navbar-inclusion").innerHTML = navbar;
 
         let filename = document.location.pathname.match(/[^\/]+$/);
         
@@ -34,6 +37,33 @@ $(document).ready(function () {
 
         // check for the first time if the navbar needs to be hidden
         handleResize();
+
+        // $('#navContainer').animate( {
+        //     'top': 0,
+        //     'left': -300
+        // }, 200);
+
+        // // add handlers here;
+        // $('#header-img').on( 'mouseout', function() {
+        //     if (!navHide)
+        //     {
+        //         $('#navContainer').animate( {
+        //             'top': 0,
+        //             'left': -300
+        //         }, 300);
+        //         navHide = true;
+        //     }
+        // });
+
+        // $('#header-img').on( 'mouseenter', function() {
+        //     $('#navContainer').animate( {
+        //         'top': 0,
+        //         'left': 0
+        //     }, 300,  function() {
+        //         navHide = false;
+        //     });
+            
+        // });
 
     }, 'text');
 
