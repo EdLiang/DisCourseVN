fb90b2f7665a18c7b7139dc0314e814cc57db431
Edward Liang <kentoruya@gmail.com>
2019-03-04 13:47:58 -0800

    Update About.js

--- js/About.js
+++ js/About.js
@@ -1,6 +1,6 @@
 function parallex() {
     ypos = window.pageYOffset;
     image = document.getElementById('parallex');
-    image.style.top = ypos * -0.9 + 'px';
+    image.style.top = ypos * -0.7 + 'px';
 }
 window.addEventListener('scroll', parallex);
